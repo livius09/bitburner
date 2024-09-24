35760ba6376f69c5cefac3ab70b9117929140aee
livius09 <levi.wimroither@hotmail.com>
2024-09-24 13:18:18 +0200

    Added console output

--- spread.js
+++ spread.js
@@ -16,10 +16,12 @@ export async function main(ns) {
   for (let i = 0; i < ar.length; i++) {
     let server = ar[i];
     await ns.nuke(server);  // Gain root access
+    ns.tprint("infecded "+server)
     await ns.scp("farm.js", server);// Copy farm.js to the server
     await ns.scp("spread.js",server);//copy farm.js to the server
     ns.exec("spread.js",server,1,server);//run spread 
     await ns.sleep(5000) //waiting for spread to finish 
     ns.exec("farm.js", server, 1, server);  // Run farm.js with 1 thread, passing the server name as an argument
+    ns.tprint("stating to farm "+server)
   }
 }
