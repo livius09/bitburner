0bcd5771339a82e4e2fa47751bb6e28e8eb2cfe1
livius09 <levi.wimroither@hotmail.com>
2024-09-25 17:32:01 +0200

    added ram upgrades

--- hacknet.js
+++ hacknet.js
@@ -2,23 +2,33 @@
 export async function main(ns) {
   var maxno = 20;//max nodes
   var maxle = 100;//max level of each node
+  var maxram = 32 //max GB ram
 
   while (true) {
     let chan = 0;//changes made
     let nodes = ns.hacknet.numNodes();
-    
+
     if (ns.hacknet.getPurchaseNodeCost() < (ns.getServerMoneyAvailable("home") - 1000) && nodes < maxno) { //if you can aford it and if not over maxno
-      ns.tprint("bought "+ns.hacknet.purchaseNode())//purchase and print its index
+      ns.tprint("bought " + ns.hacknet.purchaseNode())//purchase and print its index
       continue;//if it bought something restart the while loop
     }
 
     for (let i = 0; i < nodes; i++) {//for each node 
-      if (ns.hacknet.getNodeStats(i).level <= maxle && ns.hacknet.getLevelUpgradeCost(i) < ns.getServerMoneyAvailable("home")-1000) {
-        ns.hacknet.upgradeLevel(i,1);//Purchase a level upgrade for the node i 
-        ns.tprint("upgraded level of "+i)
-        chan += 1 
+      if (ns.hacknet.getNodeStats(i).level <= maxle && ns.hacknet.getLevelUpgradeCost(i) < ns.getServerMoneyAvailable("home") - 1000) {
+        ns.hacknet.upgradeLevel(i, 1);//Purchase a level upgrade for the node i 
+        ns.tprint("upgraded level of " + i)
+        chan += 1;
       }
     }
-    if (chan==0) {break;}
+
+    for (let j = 0; j < nodes; j++) {//for each node 
+      if (ns.hacknet.getNodeStats(j).ram <= maxram && ns.hacknet.getRamUpgradeCost(j) < ns.getServerMoneyAvailable("home") - 1000) {
+        ns.hacknet.upgradeRam(j, 1);//Purchase a ram upgrade for the node i 
+        ns.tprint("upgraded ram of " + j)
+        chan += 1;
+      }
+    }
+
+    if (chan == 0) { break; }
   }
 }
