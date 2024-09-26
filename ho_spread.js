11a47c253f09fa4e80115f13f5aa844f510304cb
livius09 <levi.wimroither@hotmail.com>
2024-09-26 14:35:25 +0200

    Fixed a breaking if, fixed order of starting farming

--- ho_spread.js
+++ ho_spread.js
@@ -1,5 +1,3 @@
-ho_spread.js
-
 /** @param {NS} ns */
 export async function main(ns) {
   var ar = ns.scan("home");  // Start by scanning from 'home'
@@ -29,36 +27,34 @@ export async function main(ns) {
   async function stfarm(ns, server) {
     await ns.nuke(server);
     ns.tprint("infected " + server);
+    await ns.scp("farm.js", server)
     ns.exec("farm.js", server, 1, server);
   }
 
   for (let a = 0; a < ar.length; a++) {
     let server = ar[a];
-    if (!ns.hasRootAccess(server) && ns.getServerRequiredHackingLevel(server) <= ns.getHackingLevel) {
+    if (!ns.hasRootAccess(server) && ns.getServerRequiredHackingLevel(server) <= ns.getHackingLevel()) {
       switch (ns.getServerNumPortsRequired(server)) {
         case 0:
           await stfarm(ns, server);
-          await ns.scp("farm.js", server);
           break;
         case 1:
-          if (ns.fileExists("BruteSSH.exe")){
+          if (ns.fileExists("BruteSSH.exe")) {
             await ns.brutessh(server);
-            await ns.scp("farm.js", server);
             await stfarm(ns, server);
-          }else{
+          } else {
             ns.tprint("you dont have the programs to crack 1 port");
           }
           break;
         case 2:
-          if (ns.fileExists("BruteSSH.exe")&&ns.fileExists("FTPCrack.exe")){
+          if (ns.fileExists("BruteSSH.exe") && ns.fileExists("FTPCrack.exe")) {
             await ns.brutessh(server);
             await ns.ftpcrack(server);
-            await ns.scp("farm.js", server);
             await stfarm(ns, server);
-          }else{
+          } else {
             ns.tprint("you dont have the programs to crack 2 ports");
           }
-        
+
         default:
           break;
       }
