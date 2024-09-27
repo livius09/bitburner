1bdb53965f7f31e2c7907a0c8f44e7a2c8f1180a
livius09 <levi.wimroither@hotmail.com>
2024-09-27 14:09:15 +0200

    Pasted control.js

--- afarm/control.js
+++ afarm/control.js
@@ -1,1 +1,32 @@
+control.js
 
+/** @param {NS} ns */
+export async function main(ns) {
+  let targ = ns.args[0];
+  while (true) {
+    //get the security and money
+    let minsec = ns.getServerMinSecurityLevel(targ);
+    let seclev = ns.getServerSecurityLevel(targ);
+    let maxmon = ns.getServerMaxMoney(targ);
+    let curmon = ns.getServerMoneyAvailable(targ);
+    //get the times 
+    let thack = ns.getHackTime(targ);
+    let tgrow = ns.getGrowTime(targ);
+    let tweak = ns.getWeakenTime(targ);
+
+    if (seclev < minsec + 5) {
+      ns.print("Runing Weaken");
+      ns.run("weaken.js");
+      await ns.sleep(thack + 100);
+    } else if (curmon < maxmon * 0.5) {
+      ns.print("Runing Grow");
+      ns.run("grow.js");
+      await ns.sleep(tgrow + 100)
+    } else {
+      ns.print("Runing Hack");
+      ns.run("hack.js");
+      await ns.sleep(thack + 100);
+    }
+
+  }
+}
