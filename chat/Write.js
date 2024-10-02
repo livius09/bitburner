8006fd9e816c9acc78eb768b46afa5166ae8b9a1
livius09 <levi.wimroither@hotmail.com>
2024-10-02 14:06:28 +0200

    Create Write.js

---
+++ chat/Write.js
@@ -0,0 +1,8 @@
+/** @param {NS} ns */
+export async function main(ns) {
+  let server = ns.args[0];
+  let mesg = ns.args[1];
+  if(ns.serverExists(server)){
+  ns.exec("reciv.js",server,1,mesg);
+  }
+}
