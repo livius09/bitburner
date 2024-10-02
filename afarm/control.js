c0b05bdfebec209edaabfa5851bd2474fcb6ed58
livius09 <levi.wimroither@hotmail.com>
2024-10-02 17:13:46 +0200

    Added ifs so that it does not crash on start

--- afarm/control.js
+++ afarm/control.js
@@ -1,32 +1,38 @@
 /** @param {NS} ns */
 export async function main(ns) {
   let targ = ns.args[0];
+  if (ns.getServerMaxMoney(targ) == 0) {
+    throw ("server " + targ + " has no money");
+  }
   while (true) {
-    //get the security and money
+    // get the security and money
     let minsec = ns.getServerMinSecurityLevel(targ);
     let seclev = ns.getServerSecurityLevel(targ);
     let maxmon = ns.getServerMaxMoney(targ);
     let curmon = ns.getServerMoneyAvailable(targ);
-    //get the times 
+
+    // get the times
     let thack = ns.getHackTime(targ);
     let tgrow = ns.getGrowTime(targ);
     let tweak = ns.getWeakenTime(targ);
-    //get max threads
-    let  free= ns.getServerMaxRam(targ)-ns.getServerUsedRam(targ);
-    let mthhack=Math.floor(free/ns.getScriptRam("hack.js"));
-    let mthweak=Math.floor(free/ns.getScriptRam("weaken.js"));
-    let mthgrow=Math.floor(free/ns.getScriptRam("grow.js"));
-    
+    // get max threads
+    let free = ns.getServerMaxRam(targ) - ns.getServerUsedRam(targ);
+    let mthhack = Math.floor(free / ns.getScriptRam("hack.js"));
+    let mthweak = Math.floor(free / ns.getScriptRam("weaken.js"));
+    let mthgrow = Math.floor(free / ns.getScriptRam("grow.js"));
 
-    if (seclev > minsec + 0.5) {
-      ns.run("weaken.js",mthweak,targ);
+    if (seclev > minsec + 0.5 && mthweak != 0 && !ns.scriptRunning("weaken.js", targ)) {
+      ns.run("weaken.js", mthweak, targ);
       await ns.sleep(tweak);
-    } else if (curmon < maxmon*0.5) {
-      ns.run("grow.js",mthgrow,targ);
+    } else if (curmon < maxmon * 0.5 && mthgrow != 0 && !ns.scriptRunning("grow.js", targ)) {
+      ns.run("grow.js", mthgrow, targ);
+      ns.run("grow.js", mthgrow, targ);
       await ns.sleep(tgrow);
-    } else {
-      ns.run("hack.js",mthhack,targ);
+    } else if (mthhack != 0 && !ns.scriptRunning("hack.js", targ)) {
+      ns.run("hack.js", mthhack, targ);
       await ns.sleep(thack);
+    } else {
+      await ns.sleep(2000);
     }
     await ns.sleep(100);
   }
