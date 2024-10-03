8e631caac6c38c10a5f43a445772db13e7db1728
livius09 <levi.wimroither@hotmail.com>
2024-10-03 11:28:43 +0200

    changed it so home wont update itself

--- update.js
+++ update.js
@@ -1,20 +1,19 @@
-/** @param {NS} ns */
 export async function main(ns) {
   let script = ns.args[0];
-  let ru= ns.args[1];
-  
+  let ru = ns.args[1];
+
   var ar = ns.scan("home");  // Start by scanning from 'home'
-  
+
   while (true) {
     let chan = 0;  // Track if any new servers are found in this loop
-    
+
     for (let i = 0; i < ar.length; i++) {
       let server = ar[i];  // Existing server in the list
       let sca = ns.scan(server);  // Scan the connected servers to this one
-      
+
       for (let j = 0; j < sca.length; j++) {  // Loop through newly scanned servers
         let newServer = sca[j];
-        
+
         // Add the newly discovered server if it's not already in the list
         if (!ar.includes(newServer)) {
           ar.push(newServer);
@@ -30,15 +29,15 @@ export async function main(ns) {
   }
 
 
-  for (let i=0;i<ar.length;i++){
-    let server=ar[i];
-    if (ns.hasRootAccess(server)==true){
-      await ns.scp(script,server);
-      if(ru==1||ru==2){
-        if(ru==2){
-        ns.killall(server);
+  for (let i = 0; i < ar.length; i++) {
+    let server = ar[i];
+    if (ns.hasRootAccess(server) == true && server != "home" ) {
+      await ns.scp(script, server);
+      if (ru == 1 || ru == 2) {
+        if (ru == 2) {
+          ns.killall(server);
         }
-        await ns.exec(script,server,1,server);
+        await ns.exec(script, server, 1, server);
       }
     }
   }
