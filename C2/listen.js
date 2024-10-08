c7c86234b7b0bd1047aacd857ab019cbe11e2a31
livius09 <levi.wimroither@hotmail.com>
2024-10-08 07:46:34 +0200

    Added input sanatazition

--- C2/listen.js
+++ C2/listen.js
@@ -1,17 +1,16 @@
 /** @param {NS} ns */
 export async function main(ns) {
-  
+
   let ten = ns.getPortHandle(10);
 
   function eror(ns) {
     ns.tprint("system eror");
     ns.kill(ns.pid);
   }
-  async function awinput(ns){
-    while (true){
-      if (!ten.empty()){
+  async function awinput(ns) {
+    while (true) {
+      if (!ten.empty()) {
         return ten.read();
-        break;
       }
       await ns.sleep(100);
     }
@@ -33,7 +32,7 @@ export async function main(ns) {
 
   if (ns.fileExists("auth.txt")) {
     let authtxt = ns.read("auth.txt");
-    var authl = authtxt.split(",");
+    var authl = authtxt.split(",").map(Number);
   } else {
     eror(ns)
   }
@@ -88,19 +87,41 @@ export async function main(ns) {
       if (rea == "add user") {
         if (authlev < 1) {
           while (true) {
-            let aduser = await ns.prompt("name of user to add", { type: "text" });
+            ns.tprint("Please enter the name of the user to add: ");
+            let aduser = await awinput(ns);
             if (!users.includes(aduser)) {
-              ns.write("user.txt", ","+aduser, "a");
-              break;
+              if (!aduser.includes(",")) {
+                ns.write("user.txt", "," + aduser, "a");
+                break;
+              } else {
+                ns.tprint("you cant have a , in user name");
+                continue;
+              }
+
             } else {
               ns.tprint("user already exists chose a other name");
             }
           }
-          let addpas = await ns.prompt("chose a pasword for user", { type: "text" });
-          ns.write("psw.txt", "," + addpas, "a");
-
-          let addauth = await ns.prompt("chose auth level for new user", { type: "text" });
-          ns.write("auth.txt", "," + addauth, "a");
+          while (true) {
+            ns.tprint("chose a pasword for the user: ");
+            let addpas = await awinput(ns);
+            if (!addpas.includes(",")) {
+              ns.write("psw.txt", "," + addpas, "a");
+              break;
+            } else {
+              ns.tprint("You cant have a , in a password");
+            }
+          }
+          while (true) {
+            ns.tprint("Chose an autlevel for the user(must be a number)");
+            var addauth = Number(await awinput(ns));
+            if (!isNaN(addauth)) {
+              ns.write("auth.txt", "," + addauth, "a");
+              break;
+            } else {
+              ns.tprint("auth level has to be a number");
+            }
+          }
 
           ns.tprint("finished seting up user you can log in after reboot");
 
