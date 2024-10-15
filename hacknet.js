b1db67c8a6e6240a271ff9ed7abdb5e015cd221e
livius09 <levi.wimroither@hotmail.com>
2024-10-15 21:04:10 +0200

    Added core upcgrades

--- hacknet.js
+++ hacknet.js
@@ -1,8 +1,8 @@
-/** @param {NS} ns */
 export async function main(ns) {
-  var maxno = 20;//max nodes
-  var maxle = 100;//max level of each node
-  var maxram = 32 //max GB ram
+  var maxno = 25;//max nodes
+  var maxle = 150;//max level of each node
+  var maxram = 32; //max GB ram
+  var maxcor = 15;
 
   while (true) {
     let chan = 0;//changes made
@@ -14,7 +14,7 @@ export async function main(ns) {
     }
 
     for (let i = 0; i < nodes; i++) {//for each node 
-      if (ns.hacknet.getNodeStats(i).level <= maxle && ns.hacknet.getLevelUpgradeCost(i) < ns.getServerMoneyAvailable("home") - 1000) {
+      if (ns.hacknet.getNodeStats(i).level < maxle && ns.hacknet.getLevelUpgradeCost(i) < ns.getServerMoneyAvailable("home") - 1000) {
         ns.hacknet.upgradeLevel(i, 1);//Purchase a level upgrade for the node i 
         ns.tprint("upgraded level of " + i)
         chan += 1;
@@ -22,10 +22,17 @@ export async function main(ns) {
     }
 
     for (let j = 0; j < nodes; j++) {//for each node 
-      if (ns.hacknet.getNodeStats(j).ram <= maxram && ns.hacknet.getRamUpgradeCost(j) < ns.getServerMoneyAvailable("home") - 1000) {
+      if (ns.hacknet.getNodeStats(j).ram < maxram && ns.hacknet.getRamUpgradeCost(j) < ns.getServerMoneyAvailable("home") - 1000) {
         ns.hacknet.upgradeRam(j, 1);//Purchase a ram upgrade for the node i 
         ns.tprint("upgraded ram of " + j)
         chan += 1;
+      }
+    }
+    for (let u = 0; u < nodes; u++) {
+      if (ns.hacknet.getNodeStats(u).cores < maxcor && ns.hacknet.getCoreUpgradeCost(u) < ns.getServerMoneyAvailable("home") - 100) {
+        ns.hacknet.upgradeCore(u, 1);
+        ns.tprint("upgraded cores of " + u);
+        chan += 1
       }
     }
 
